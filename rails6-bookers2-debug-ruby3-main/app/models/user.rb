9b27c76f5603a1e6f08bbdc912d5dccfce0c64a8
EC2 Default User <ec2-user@ip-172-31-33-11.ap-northeast-1.compute.internal>
2022-12-26 06:50:26 +0000

    [Update]Advanced_Task3

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -5,19 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_many :bool_comments, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
-
+  validates :introduction, length: {maximum: 50}
 
   def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 end
--- rails6-bookers2-debug-ruby3-main/db/schema.rb
+++ rails6-bookers2-debug-ruby3-main/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_070248) do
+ActiveRecord::Schema.define(version: 2022_12_26_053540) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
